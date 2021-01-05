56369c19486ab4fff0bb38f03dd3a7440b73c306
illias <illiasibrahim6@gmail.com>
2021-01-05 23:13:20 +0530

    added one entry and two buttons-positioning failed

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
-i = "illias"
+from tkinter import *
+
+window = Tk()
+
+window.title("Calculator")
+window.geometry("300x400")
+
+entry_field = Entry(window, width=45, justify=RIGHT)
+entry_field.grid(column=0, row=0, columnspan=4,  pady=8, padx=13, ipady=8)
+
+def helloCallBack():
+   print( "Hello Python")
+
+
+one_button = Button(text='1', command=helloCallBack())
+one_button.grid(column=0, row=1, padx=13)
+
+two_button = Button(text='2', command=helloCallBack())
+one_button.grid(column=1, row=1)
+
+
+
+
+window.mainloop()
+
+
